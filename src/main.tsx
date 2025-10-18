318424520f2d4cc3a784ed939e4db526ed0e47b4
[dyad] <aymmej1920@gmail.com>
2025-10-18 01:27:43 +0100

    [dyad] Suppression de la fonctionnalité du mode sombre de l'application. - wrote 3 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,12 @@ import App from './App.tsx';
 import './index.css';
 import { SessionContextProvider } from './components/SessionContextProvider.tsx';
 import ToastProvider from './components/ToastProvider.tsx';
-import { ThemeProvider } from './components/ThemeProvider.tsx'; // Import ThemeProvider
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider> {/* Wrap the entire app with ThemeProvider */}
-      <SessionContextProvider>
-        <ToastProvider />
-        <App />
-      </SessionContextProvider>
-    </ThemeProvider>
+    <SessionContextProvider>
+      <ToastProvider />
+      <App />
+    </SessionContextProvider>
   </StrictMode>
 );
