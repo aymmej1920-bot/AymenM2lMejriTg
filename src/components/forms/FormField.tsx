fb659d4425876469cb1209a1e1afb1095d32373a
[dyad] <aymmej1920@gmail.com>
2025-10-19 17:37:33 +0100

    [dyad] Correction de l'erreur de compilation TypeScript - wrote 1 file(s)

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useFormContext, Controller, FieldValues, Path } from 'react-hook-form';
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
