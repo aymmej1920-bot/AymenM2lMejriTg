d90676b384e46ba1f619a83097d94f42dfb75549
[dyad] <aymmej1920@gmail.com>
2025-10-25 18:54:17 +0100

    [dyad] wrote 1 file(s)

--- src/components/forms/DatePickerField.tsx
+++ src/components/forms/DatePickerField.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'; // Import default styles
 import { useFormContext, Controller, Path, FieldValues } from 'react-hook-form';
